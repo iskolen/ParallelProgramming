33bfb5469a2a88e47e52ce471884eb2f95c11928
Vadim <iskolen@mail.ru>
2023-09-19 00:57:45 +0300

    I did my first lab work

---
+++ Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,48 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+	printf("Поток №%d выполняет свою работу\n", *(int*)lpParam);
+	ExitThread(0);
+}
+
+int main(int argc, CHAR* argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	if (argc != 2)
+	{
+		std::cerr << "LabTest1.exe <количество потоков>" << std::endl;
+		return -1;
+	}
+
+	int numThreads = atoi(argv[1]);
+
+	if (numThreads < 0)
+	{
+		std::cerr << "Количество потоков не может быть отрицательным!" << std::endl;
+		return -2;
+	}
+
+	HANDLE* handles = new HANDLE[numThreads];
+	int* threadNumbers = new int[numThreads];
+
+	for (int i = 0; i < numThreads; i++)
+	{
+		threadNumbers[i] = i;
+		handles[i] = CreateThread(NULL, 0, &ThreadProc, &threadNumbers[i], CREATE_SUSPENDED, NULL);
+	}
+
+	for (int m = 0; m < numThreads; m++)
+		ResumeThread(handles[m]);
+
+	WaitForMultipleObjects(numThreads, handles, true, INFINITE);
+
+	for (int i = 0; i < numThreads; i++)
+		CloseHandle(handles[i]);
+
+	return 0;
+}
