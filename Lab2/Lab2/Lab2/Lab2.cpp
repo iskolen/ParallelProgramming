285cf84fd46fbe8d5ec952c11357ef547f17ff58
Vadim <iskolen@mail.ru>
2023-10-10 00:15:29 +0300

    Сделал 2-ую лабу

--- Lab2/Lab2/Lab2/Lab2.cpp
+++ Lab2/Lab2/Lab2/Lab2.cpp
@@ -3,121 +3,172 @@
 #include <cstdint>
 #include <vector>
 #include <thread>
-#include "windows.h"
+#include <chrono>
+#include <string>
+#include <Windows.h>
 
-#pragma pack(push, 1) // Отключаем выравнивание байтов
+#pragma pack(push, 1)
+struct BMPHeader
+{
+	uint16_t fileType;
+	uint32_t fileSize;
+	uint16_t reserved1;
+	uint16_t reserved2;
+	uint32_t dataOffset;
+	uint32_t headerSize;
+	int32_t width;
+	int32_t height;
+	uint16_t planes;
+	uint16_t bitsPerPixel;
+	uint32_t compression;
+	uint32_t imageSize;
+	int32_t xPixelsPerMeter;
+	int32_t yPixelsPerMeter;
+	uint32_t colorsUsed;
+	uint32_t colorsImportant;
+};
+#pragma pack(pop)
 
-// Структура для заголовка BMP файла
-struct BMPHeader {
-	uint16_t fileType;        // Тип файла, должно быть 'BM' (0x4D42)
-	uint32_t fileSize;       // Размер файла в байтах
-	uint16_t reserved1;       // Зарезервировано (0)
-	uint16_t reserved2;       // Зарезервировано (0)
-	uint32_t dataOffset;      // Смещение до начала данных в байтах
-	uint32_t headerSize;      // Размер заголовка в байтах
-	int32_t width;            // Ширина изображения в пикселях
-	int32_t height;           // Высота изображения в пикселях
-	uint16_t planes;          // Количество плоскостей, должно быть 1
-	uint16_t bitsPerPixel;    // Глубина цвета (бит на пиксель)
-	uint32_t compression;     // Метод сжатия, 0 для несжатых изображений
-	uint32_t imageSize;       // Размер изображения в байтах
-	int32_t xPixelsPerMeter;  // Горизонтальное разрешение, пикселей на метр
-	int32_t yPixelsPerMeter;  // Вертикальное разрешение, пикселей на метр
-	uint32_t colorsUsed;      // Количество используемых цветов (0 для 24-битных изображений)
-	uint32_t colorsImportant; // Количество важных цветов (0)
+struct ThreadData
+{
+	std::vector<uint8_t>& imageData;
+	int startColumn;
+	int endColumn;
+	int imageWidth;
+	int imageHeight;
 };
 
-#pragma pack(pop) // Восстанавливаем выравнивание байтов
-
-// Функция для размытия вертикальной полосы изображения
-void BlurVerticalStrip(std::vector<uint8_t>& imageData, int width, int height, int startColumn, int endColumn, BMPHeader header)
+std::vector<uint8_t> ReadBmpImage(const char* fileName, BMPHeader& header)
 {
-	int bytesPerPixel = header.bitsPerPixel / 8;
+	std::ifstream bmpFile(fileName, std::ios::binary);
+	bmpFile.read(reinterpret_cast<char*>(&header), sizeof(BMPHeader));
 
-	for (int column = startColumn; column < endColumn; ++column) {
-		for (int row = 0; row < height; ++row) {
-			int pixelOffset = (row * width + column) * bytesPerPixel;
+	std::vector<uint8_t> imageData(header.fileSize - header.dataOffset);
+	bmpFile.read(reinterpret_cast<char*>(imageData.data()), imageData.size());
+	bmpFile.close();
 
-			if (column > 0 && column < width - 1) {
-				// Усреднение цветов соседних пикселей по горизонтали
-				for (int b = 0; b < bytesPerPixel; ++b) 
+	return imageData;
+}
+
+void ApplyGaussianBlur(void* threadData)
+{
+	ThreadData* data = reinterpret_cast<ThreadData*>(threadData);
+	std::vector<uint8_t>& imageData = data->imageData;
+	int startColumn = data->startColumn;
+	int endColumn = data->endColumn;
+	int imageHeight = data->imageHeight;
+	int imageWidth = data->imageWidth;
+
+	const int kernelSize = 5;
+	double sigma = 1.0;
+
+	double kernel[kernelSize][kernelSize];
+	double kernelSum = 0.0;
+	int radius = kernelSize / 2;
+
+	for (int i = -radius; i <= radius; ++i)
+	{
+		for (int j = -radius; j <= radius; ++j)
+		{
+			double x = i * 1.0;
+			double y = j * 1.0;
+			kernel[i + radius][j + radius] = exp(-(x * x + y * y) / (2.0 * sigma * sigma)) / (2.0 * 3.14 * sigma * sigma);
+			kernelSum += kernel[i + radius][j + radius];
+		}
+	}
+
+	for (int i = 0; i < kernelSize; ++i)
+	{
+		for (int j = 0; j < kernelSize; ++j)
+		{
+			kernel[i][j] /= kernelSum;
+		}
+	}
+
+	for (int y = 0; y < imageHeight; ++y)
+	{
+		for (int x = startColumn; x < endColumn; ++x)
+		{
+			double blurredPixelR = 0.0;
+			double blurredPixelG = 0.0;
+			double blurredPixelB = 0.0;
+
+			for (int i = -radius; i <= radius; ++i)
+			{
+				for (int j = -radius; j <= radius; ++j)
 				{
-					uint32_t sum = 0;
+					int xOffset = x + i;
+					int yOffset = y + j;
 
-					// Суммируем значения цветов соседних пикселей
-					for (int x = -1; x <= 1; ++x) {
-						int neighborColumn = column + x;
-						int neighborPixelOffset = (row * width + neighborColumn) * bytesPerPixel;
-
-						if (neighborColumn >= 0 && neighborColumn < width) {
-							sum += imageData[neighborPixelOffset + b];
-						}
+					if (xOffset >= 0 && xOffset < imageWidth && yOffset >= 0 && yOffset < imageHeight)
+					{
+						int pixelIndex = (yOffset * imageWidth + xOffset) * 3;
+						blurredPixelR += imageData[pixelIndex] * kernel[i + radius][j + radius];
+						blurredPixelG += imageData[pixelIndex + 1] * kernel[i + radius][j + radius];
+						blurredPixelB += imageData[pixelIndex + 2] * kernel[i + radius][j + radius];
 					}
-
-					// Усредняем и записываем значение в текущий пиксель
-					imageData[pixelOffset + b] = static_cast<uint8_t>(sum / 3); // Усреднение по 3 пикселям
 				}
 			}
+
+			int pixelIndex = (y * imageWidth + x) * 3;
+			imageData[pixelIndex] = static_cast<uint8_t>(blurredPixelR);
+			imageData[pixelIndex + 1] = static_cast<uint8_t>(blurredPixelG);
+			imageData[pixelIndex + 2] = static_cast<uint8_t>(blurredPixelB);
 		}
 	}
 }
 
-int main() 
+void SaveBmp(const char* fileName, const BMPHeader& header, const std::vector<uint8_t>& imageData)
+{
+	std::ofstream outFile(fileName, std::ios::binary);
+	outFile.write(reinterpret_cast<const char*>(&header), sizeof(BMPHeader));
+	outFile.write(reinterpret_cast<const char*>(imageData.data()), imageData.size());
+	outFile.close();
+}
+
+int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "Russian");
-	const char* inputBmpFileName = "input.bmp";
-	const char* outputBmpFileName = "output.bmp";
-	std::ifstream bmpFile(inputBmpFileName, std::ios::binary);
-
-	if (!bmpFile) 
+	if (argc != 5)
 	{
-		std::cerr << "Ошибка при открытии файла." << std::endl;
+		std::cerr << "Использование: Lab2.exe <input.bmp> <output.bmp> <numCores> <numThreads>" << std::endl;
 		return 1;
 	}
+
+	const char* inputBmpFileName = argv[1];
+	const char* outputBmpFileName = argv[2];
+	int numCores = std::stoi(argv[3]);
+	int numThreads = std::stoi(argv[4]);
+
+	auto startTime = std::chrono::high_resolution_clock::now();
 
 	BMPHeader header;
-	bmpFile.read(reinterpret_cast<char*>(&header), sizeof(BMPHeader));
+	std::vector<uint8_t> imageData = ReadBmpImage(inputBmpFileName, header);
 
-	if (header.fileType != 0x4D42)
-	{
-		std::cerr << "Неподдерживаемый формат BMP файла." << std::endl;
-		return 1;
-	}
-
-	std::vector<uint8_t> imageData(header.fileSize - header.dataOffset);
-	bmpFile.read(reinterpret_cast<char*>(imageData.data()), imageData.size());
-
-	bmpFile.close();
-
-	int numThreads = 4;
-
-	std::vector<std::thread> threads;
+	HANDLE* handles = new HANDLE[numThreads];
 	int columnsPerThread = header.width / numThreads;
-	int remainingColumns = header.width % numThreads;
+	int extraColumns = header.width % numThreads;
 	int startColumn = 0;
 
-	for (int i = 0; i < numThreads; ++i) 
+	for (int i = 0; i < numThreads; ++i)
 	{
-		int endColumn = startColumn + columnsPerThread;
-		if (i < remainingColumns) 
-		{
-			endColumn++; // Равномерное распределение остатка
-		}
-		threads.emplace_back(BlurVerticalStrip, std::ref(imageData), header.width, header.height, startColumn, endColumn);
-		startColumn = endColumn;
+		int start = i * columnsPerThread;
+		int end = (i == numThreads - 1) ? (start + columnsPerThread + extraColumns) : (start + columnsPerThread);
+		handles[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ApplyGaussianBlur,
+			new ThreadData{ std::ref(imageData), start, end, header.width, header.height }, 0, NULL);
+
+		SetThreadAffinityMask(handles[i], (1ULL << numCores) - 1);
 	}
 
-	for (auto& thread : threads) 
+	for (int i = 0; i < numThreads; i++)
 	{
-		thread.join();
+		ResumeThread(handles[i]);
 	}
+	WaitForMultipleObjects(numThreads, handles, true, INFINITE);
 
-	std::ofstream outputFile(outputBmpFileName, std::ios::binary);
-	outputFile.write(reinterpret_cast<const char*>(&header), sizeof(BMPHeader));
-	outputFile.write(reinterpret_cast<const char*>(imageData.data()), imageData.size());
-	outputFile.close();
+	SaveBmp(outputBmpFileName, header, imageData);
 
-	std::cout << "Изображение успешно обработано и сохранено в " << outputBmpFileName << std::endl;
-
-	return 0;
+	auto endTime = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = endTime - startTime;
+	std::cout << "Время выполнения: " << elapsed.count() << " секунд." << std::endl;
 }
